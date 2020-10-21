f03fa012945de534b5088fe062db47a27b7c876a
Jonatas6199 <jojokl@hotmail.com>
2020-10-20 23:52:54 -0300

    add  CORS to server

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 require('rootpath')();
+const cors = require('cors');
+
 const express = require('express'),
   app = express(),
   bodyParser = require('body-parser'),
   port = process.env.PORT || 5000
-const cors = require('cors');
-app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/tag', require('api/tag'));
 app.use('/user', require('api/user'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
