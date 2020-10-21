7bb2b82af5de2aeae57172ab94783e42eb0f7b5d
Jonatas6199 <jojokl@hotmail.com>
2020-10-20 23:21:00 -0300

    alterações nas configs da API

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const express = require('express'),
   app = express(),
   bodyParser = require('body-parser'),
   port = process.env.PORT || 5000
-
+const cors = require('cors');
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
