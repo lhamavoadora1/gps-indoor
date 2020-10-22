0537e935e615719b1c28266ac69aa3ef2e7b09e6
Jonatas6199 <jojokl@hotmail.com>
2020-10-22 20:41:57 -0300

    troca cons por var

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require('rootpath')();
-const cors = require('cors');
+var express = require('express');
+var cors = require('cors');
 
-const express = require('express'),
-  app = express(),
-  bodyParser = require('body-parser'),
-  port = process.env.PORT || 5000
+
+var app = express();
+app.use(cors());
+
+var bodyParser = require('body-parser'),
+port = process.env.PORT || 5000
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,10 +15,5 @@ app.use(bodyParser.json());
 app.use('/tag', require('api/tag'));
 app.use('/user', require('api/user'));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
