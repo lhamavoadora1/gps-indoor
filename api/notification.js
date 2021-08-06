6a564207523c5b9c7a37539a3ede3d2f52450b7f
lhamavoadora1 <53661607+lhamavoadora1@users.noreply.github.com>
2021-08-05 22:50:00 -0300

    send notification from broker to mongo

--- api/notification.js
+++ api/notification.js
@@ -6,21 +6,32 @@ const express = require('express'),
 var router = express.Router();
 router.get('/', getAllNotifications);
 router.get('/:timestamp', getNotification);
-router.post('/', insertNotification);
+// router.post('/', insertNotification);
 // router.delete('/:timestamp', deleteNotification);
 module.exports = router;
 
 async function getAllNotifications(req, res) {
     var authorization = req.headers.authorization;
     if (await mongo.authenticate(authorization)) {
+        var sensor_id_list = [];
         var ownerKey = utils.getOwnerKey(authorization);
-        var notificationsRetrieved = await mongo.findDB(collection, {
+        var sensorsRetrieved = await mongo.findDB('sensors', {
             owner: ownerKey
         });
-        if (!utils.isEmpty(notificationsRetrieved)) {
-            res.send({
-                notificationsRetrieved
+        if (!utils.isEmpty(sensorsRetrieved)) {
+            sensorsRetrieved.forEach(function (item, index, array) {
+                sensor_id_list.push({sensor_id:item.sensor_id});
             });
+            var notificationsRetrieved = await mongo.findDB(collection, {
+                $or: sensor_id_list
+            });
+            if (!utils.isEmpty(notificationsRetrieved)) {
+                res.send({
+                    notificationsRetrieved
+                });
+            } else {
+                res.status(204).send();
+            }
         } else {
             res.status(204).send();
         }
@@ -34,12 +45,12 @@ async function getNotification(req, res) {
         var timestamp = Number(req.params.timestamp);
         var authorization = req.headers.authorization;
         if (await mongo.authenticate(authorization)) {
-            var ownerKey = utils.getOwnerKey(authorization);
+            // var ownerKey = utils.getOwnerKey(authorization);
             var notificationsRetrieved = await mongo.findDB(collection, {
                 timestamp: {
                     $gt: timestamp
-                },
-                owner: ownerKey
+                }
+                // owner: ownerKey
             });
             if (!utils.isEmpty(notificationsRetrieved)) {
                 res.send({
@@ -57,39 +68,24 @@ async function getNotification(req, res) {
     }
 }
 
-async function insertNotification(req, res) {
+async function insertNotification(fullRequest) {
     try {
-        var authorization = req.headers.authorization;
-        if (await mongo.authenticate(authorization)) {
-            // APENAS O USER PADRÃO DO BROKER PODE NOTIFICAR!!!!!!!!!!
-            var timestamp = req.body.timestamp;
-            if (utils.isEmpty(timestamp)) {
-                res.status(400).send(new utils.Error(`timestamp is empty!`));
+        var sensorsRetrieved = await mongo.findDB('sensors', {
+            sensor_id: fullRequest.sensor_id
+        });
+        if (!utils.isEmpty(sensorsRetrieved)) {
+            var data = await mongo.insertDB(collection, fullRequest);
+            if (data.result.n > 0) {
+                console.log('Tag inserted!');
             } else {
-                var ownerKey = utils.getOwnerKey(authorization);
-                var notificationsRetrieved = await mongo.findDB(collection, {
-                    timestamp: timestamp,
-                    owner: ownerKey
-                });
-                if (!utils.isEmpty(notificationsRetrieved)) {
-                    res.status(406).send(new utils.Error(`Notification '${timestamp}' is already in database!`));
-                } else {
-                    var fullRequest = new NotificationInsert(req.body);
-                    fullRequest.owner = ownerKey;
-                    var data = await mongo.insertDB(collection, fullRequest);
-                    if (data.result.n > 0) {
-                        res.status(201).send(new utils.Success(`Notification '${timestamp}' inserted!`));
-                    } else {
-                        res.status(406).send(new utils.Error(`Notification '${timestamp}' failed to insert, contact an admin for help`));
-                    }
-                }
+                console.log('Tag not inserted!');
             }
         } else {
-            res.status(401).send();
+            console.log(`No sensor ${fullRequest.sensor_id} found!`);
         }
     } catch (err) {
         console.log('Body from request may be in the wrong format:');
-        console.log(req.body)
+        console.log(fullRequest)
         console.error(err);
         res.status(500).send(new utils.Error(err));
     }
@@ -123,6 +119,56 @@ class NotificationInsert {
     constructor(notification) {
         this.tag_id     = notification.tag_id;
         this.sensor_id  = notification.sensor_id;
-        this.timestamp  = notification.timestamp;
+        this.timestamp  = new Date().getTime();
     }
 }
+
+// Broker Connection
+
+var mqtt = require('mqtt');
+var options = {
+    port: 1883
+    // clientId: 'LEITOR_001',
+    // username: 'NnB7tFj8KgQdkjg',
+    // password: 'KoTg3FGuYMMwSxW',
+};
+
+var broker = 'mqtt://52.14.171.32/';
+// 'mqtt://ioticos.org';
+var topic = 'Antenna001';
+// 'qkk2sNHLWr2MFm6/output';
+
+var client = mqtt.connect(broker, options);
+
+client.on('connect', function () {
+    console.log(`Connected to broker on ${broker}`);
+    client.subscribe(topic, function (err) {
+        if (!err) {
+            console.log(`Subscribed to ${topic}`);
+        }
+    });
+});
+
+client.on('message', async function (topic, message) {
+
+    console.log(`message from topic: ${topic}`);
+    console.log(message.toString());
+    var notification = new NotificationInsert(JSON.parse(message.toString()));
+    console.log(notification);
+
+    // var ownerKey;
+    // var sensorsRetrieved = await mongo.findDB(collection, {
+    //     sensor_id: notification.sensor_id
+    // });
+    // if (!utils.isEmpty(sensorsRetrieved)) {
+        // ownerKey = utils.getOwnerKey(authorization);
+
+    insertNotification(notification);
+
+    // } else {
+        
+    // }
+    
+    // client.end();
+
+});
--- server.js
+++ server.js
@@ -17,33 +17,3 @@ app.use('/tag', require('api/tag'));
 app.use('/user', require('api/user'));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-var mqtt = require('mqtt');
-
-var options = {
-    port: 1883,
-    clientId: 'GAPO_IDENTIFICATION_LEITOR_001',
-    username: 'NnB7tFj8KgQdkjg',
-    password: 'KoTg3FGuYMMwSxW',
-};
-
-var client = mqtt.connect('mqtt://ioticos.org', options);
-
-client.on('connect', function () {
-    client.subscribe('qkk2sNHLWr2MFm6/output', function (err) {
-        if (!err) {
-            // client.publish('test', 'Hello mqtt!!!');
-        }
-    })
-});
-
-client.on('message', function (topic, message) {
-    // message is Buffer
-    console.log(message.toString());
-    console.log({
-        tag_id: message.tag_id,
-        sensor_id: message.sensor_id,
-        timestamp: new Date().getTime()
-    })
-    // client.end();
-});
