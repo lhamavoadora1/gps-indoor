0eabe229aa0a240c3de4394ed2fe010076aa2bed
lhamavoadora1 <53661607+lhamavoadora1@users.noreply.github.com>
2021-09-16 20:36:05 -0300

    change project variables

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require('rootpath')();
 var express = require('express');
 var cors = require('cors');
+var process = require('process.json');
 
 var app = express();
 app.use(cors());
 
-var port = process.env.PORT || 5000
+var port = process.env.PORT;
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: false}));
