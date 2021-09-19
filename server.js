27d584c3b8bf4a5ff457b11d4add230fa87eac1e
Jonatas João <jojokl@hotmail.com>
2021-09-18 23:37:26 -0300

    Adiciona nova rota pra gerar pdf

--- server.js
+++ server.js
@@ -22,5 +22,6 @@ app.use('/sector', require('api/sector'));
 app.use('/sensor', require('api/sensor'));
 app.use('/tag', require('api/tag'));
 app.use('/user', require('api/user'));
+app.use('/report', require('api/report'));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
