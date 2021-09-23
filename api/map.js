3cdc7e3cdc44866f7531dc02b2f6815924381921
lhamavoadora1 <53661607+lhamavoadora1@users.noreply.github.com>
2021-09-22 22:35:47 -0300

    add getAllMaps to map subdomain

--- api/map.js
+++ api/map.js
@@ -4,11 +4,37 @@ const express = require('express'),
     collection = 'maps';
 
 var router = express.Router();
+router.get('/', getAllMaps);
 router.get('/:map_id', getMap);
 router.post('/', insertMap);
 router.patch('/:map_id', updateMap);
 router.delete('/:map_id', deleteMap);
 module.exports = router;
+
+async function getAllMaps(req, res) {
+    try {
+        var authorization = req.headers.authorization;
+        var authData = await mongo.authenticateToken(authorization);
+        if (authData) {
+            var ownerKey = authData.owner;
+            var mapsRetrieved = await mongo.findDB(collection, {
+                owner: ownerKey
+            });
+            if (!utils.isEmpty(mapsRetrieved)) {
+                res.send({
+                    mapsRetrieved
+                });
+            } else {
+                res.status(204).send();
+            }
+        } else {
+            res.status(401).send();
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(new utils.Error(err));
+    }
+}
 
 async function getMap(req, res) {
     try {
